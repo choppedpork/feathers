b73b1b9c795c26ac8cd680238fb6e216dd1e2ee9
chopped pork <solidek@gmail.com>
2022-05-12 23:40:36 +0100

    decrease bcrypt work factor to the same value used in the latest version of authentication-local as work factor of 15 takes a _lot_ of time

--- packages/authentication-local/lib/utils/hash.js
+++ packages/authentication-local/lib/utils/hash.js
@@ -1,14 +1,9 @@
 const bcrypt = require('bcryptjs');
 
-const BCRYPT_WORK_FACTOR_BASE = 12;
-const BCRYPT_DATE_BASE = 1483228800000;
-const BCRYPT_WORK_INCREASE_INTERVAL = 47300000000;
+const BCRYPT_WORK_FACTOR = 10;
 
 module.exports = function hasher (password) {
   return new Promise((resolve, reject) => {
-    let BCRYPT_CURRENT_DATE = new Date().getTime();
-    let BCRYPT_WORK_INCREASE = Math.max(0, Math.floor((BCRYPT_CURRENT_DATE - BCRYPT_DATE_BASE) / BCRYPT_WORK_INCREASE_INTERVAL));
-    let BCRYPT_WORK_FACTOR = Math.min(19, BCRYPT_WORK_FACTOR_BASE + BCRYPT_WORK_INCREASE);
 
     bcrypt.genSalt(BCRYPT_WORK_FACTOR, function (error, salt) {
       if (error) {
